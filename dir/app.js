b43c641a0fbc2f7f2fcf1e2fac2b73e31bab6331
Adrian Pearman <adrianpearman12@gmail.com>
2023-06-28 00:05:59 -0400

    adding new animations and data fetching to seperate server

--- dir/app.js
+++ dir/app.js
@@ -1,3 +1,5 @@
+// BaseURL for network requests
+const baseURL = "http://localhost:9000";
 // DOM Elements
 // Navigation
 const navigation = document.querySelector("nav");
@@ -19,7 +21,8 @@ const timelineContainerMobile = document.querySelector(
   ".timelineContainerMobile"
 );
 // RSVP
-const userIDForm = document.querySelector("#userIDForm");
+const userIDForm = document.getElementById("userIDForm");
+const invitationError = document.getElementById("invitationError");
 // Links
 const accomdationContainers = document.querySelectorAll(
   ".accomodationContainer"
@@ -27,7 +30,6 @@ const accomdationContainers = document.querySelectorAll(
 // General
 const rsvpButton = document.getElementById("rsvpButton");
 const backToTopButton = document.getElementById("backToTop");
-
 // Sections
 const headerSection = document.querySelector("header");
 const aboutUsSection = document.getElementById("aboutUs");
@@ -57,6 +59,18 @@ slides.forEach((slide, indx) => {
 });
 
 // ----- FUNCTIONS -----
+// ----- REUSEABLE FUNCTIONS -----
+const filteredArr = (array) => {
+  return array.filter((email, i) => {
+    const t =
+      i ===
+      array.findIndex((element) => {
+        return element === email;
+      });
+    return t;
+  });
+};
+// ----- ANIMATIONS -----
 //  Helper function for scrolling to element
 const scrollToElement = (e, element, applyOffset = false) => {
   const offset = applyOffset === true ? navigation.clientHeight : 0;
@@ -169,11 +183,14 @@ const showDetailSVGs = () => {
 // Animating the accomodations
 const showAccomodations = () => {
   setTimeout(() => {
-    accomdationContainers[0].classList.add("show");
+    accomdationContainers[0].classList.remove("hidden");
   }, 0);
   setTimeout(() => {
-    accomdationContainers[1].classList.add("show");
+    accomdationContainers[1].classList.remove("hidden");
   }, 600);
+  setTimeout(() => {
+    accomdationContainers[2].classList.remove("hidden");
+  }, 1200);
 };
 //  Changing active nav on scroll
 const changeActiveSection = (height) => {
@@ -264,10 +281,217 @@ const changeActiveSection = (height) => {
     backToTopButton.classList.add("show");
   }
 };
+// ----- RSVP FORM FUNCTIONS -----
+// Fetches groups from DB
+const fetchGroup = async (groupID) => {
+  const url = new URL(`${baseURL}/api/group`);
+  url.search = new URLSearchParams({ id: groupID });
+
+  try {
+    const guestData = await fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+    return await guestData.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+// Updates values from DB
+const updateGuest = async (data, id) => {
+  const url = new URL(`${baseURL}/api/update`);
+
+  const requestOptions = {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ data, id }),
+  };
+
+  try {
+    const updatedGuest = await fetch(url, requestOptions);
+    return await updatedGuest.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+// Email function
+const submitEmail = async (email) => {
+  const url = new URL(`${baseURL}/api/email`);
+
+  try {
+    const sendEmailResponse = await fetch(url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        data: email,
+      }),
+    });
+    return await sendEmailResponse.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+const userIDFormAction = async (e) => {
+  // stopping the default form action
+  e.preventDefault();
+  // accessing the value typed in
+  const invitationFormData = e.target[0].value;
+  // not allowiung form to submit with an empty value
+  if (invitationFormData === "") {
+    invitationError.innerText = "Please enter a Invitation ID";
+    invitationError.classList.remove("hidden");
+    return;
+  }
+  // accessing db to grab the groups
+  const groupData = await fetchGroup(invitationFormData);
+
+  // if there's an error with the invitation id
+  if (groupData.data === false) {
+    invitationError.innerText = groupData.msg;
+    invitationError.classList.remove("hidden");
+  } else {
+    // if there's no error with the invitation id
+    // hide invitation form on the page
+    userIDForm.classList.add("none");
+    const userConfirmationForm = document.getElementById(
+      "userConfirmationForm"
+    );
+    // show the individual guest form
+    setTimeout(() => {
+      const userConfirmationList = userConfirmationForm.children[0];
+      // template to display values on the page
+      const userTemplate = ({ id, name, email, response_going }) => {
+        const selectedOption = () => {
+          if (response_going === true || response_going === null) {
+            return `
+                  <option value="${true}" selected >
+                    Attending
+                  </option>
+                  <option value="${false}">
+                    Unable to attend
+                  </option>
+                `;
+          } else {
+            return `
+                  <option value="${true}">
+                    Attending
+                  </option>
+                  <option value="${false}" selected>
+                    Unable to attend
+                  </option>
+                `;
+          }
+        };
+
+        return `
+             <li id="${id}">
+                <div class="textContainer">
+                  <label for="name-${id}">Name: </label>
+                  <input
+                    id="name-${id}"
+                    type="text"
+                    placeholder="please enter your name"
+                    value="${name ? name : ""}"
+                  />
+                </div>
+
+                <div class="emailContainer">
+                  <label for="email-${id}">Email: </label>
+                  <input
+                    id="email-${id}"
+                    type="email"
+                    placeholder="please enter your email"
+                    value="${email ? email : ""}"
+                  />
+                </div>
+                <p class="errorMessage hidden" id="error-${id}">Please enter a valid email</p>
+                <div class="selectContainer">
+                  <select>
+                    ${selectedOption()}
+                  </select>
+                  <div class="iconContainer">
+                    <i class="fa-solid fa-caret-down"></i>
+                  </div>
+                </div>
+                </li>
+            `;
+      };
+      // generating the values to be put on the page
+      groupData.data.forEach((guest) => {
+        const guestElement = userTemplate(guest);
+        userConfirmationList.insertAdjacentHTML("beforeend", guestElement);
+      });
+      // showing the new html on the page
+      userConfirmationForm.classList.remove("hidden");
+    }, 150);
+  }
+};
+const userConfirmationFormAction = async (e) => {
+  // stopping the default action of the form
+  e.preventDefault();
+  // accessing thr values from the page
+  const guestDetails = userConfirmationForm.children[0].children;
+  // empty array to capture the emails
+  const guestEmails = [];
+  // looping through the elements on the page to grab the data
+  for (const g of guestDetails) {
+    // Gathering information from  the guest
+    const currentDate = new Date();
+    const guestID = g.id;
+    const guestName = g.children[0].children[1].value;
+    const guestEmail = g.children[1].children[1].value;
+    const guestResponse =
+      g.children[3].children[0].value.toLowerCase() === "true";
+    // Validating whether the email is empty and show casing an error msg
+    if (guestEmail === "") {
+      g.children[2].classList.remove("hidden");
+    } else {
+      g.children[2].classList.add("hidden");
+    }
+    // pushing emails to the list
+    guestEmails.push(guestEmail);
+    // Updated data obj to be sent to database
+    const data = {
+      name: guestName,
+      email: guestEmail,
+      response_going: guestResponse,
+      response_date: currentDate.toUTCString(),
+    };
+    // Sending the data to the  db
+    const guest = await updateGuest(data, guestID);
+    // Error handling relating to the submitted data
+    if (guest.success === false) {
+      alert("OOOOOOOOP");
+    }
+  }
+  // Filter out duplicates from guest email array
+  const filteredEmailList = filteredArr(guestEmails);
+
+  filteredEmailList.forEach((email) => {
+    // TODO: figure out how to send an email (sendgrid or mailgun)
+    submitEmail(email);
+  });
+
+  userConfirmationForm.classList.add("none");
+  document.querySelector(".thankYouContainer").classList.remove("hidden");
+};
 // ----- NAMESPACE OBJECT -----
 const weddingObj = {
   init: () => {
     // ----- Event Listeners -----
+    // Applying Scroll Effects
+    document.addEventListener("scroll", () => {
+      changeActiveSection(window.scrollY);
+    });
     // Navigation
     navigation.addEventListener("click", scrollToSection);
     // Mobile Navigation
@@ -283,26 +507,15 @@ const weddingObj = {
     backToTopButton.addEventListener("click", (e) => {
       scrollToSection(e);
     });
-    // Applying Scroll Effects
-    document.addEventListener("scroll", (event) => {
-      changeActiveSection(window.scrollY);
-    });
     // Carousel
     nextSlide.addEventListener("click", () => changeSlide("forward"));
     prevSlide.addEventListener("click", () => changeSlide());
     //  RSVP Forms
-    userIDForm.addEventListener("submit", (e) => {
-      e.preventDefault();
-      userIDForm.classList.add("none");
-      setTimeout(() => {
-        document
-          .getElementById("userConfirmationForm")
-          .classList.remove("hidden");
-      }, 150);
-    });
+    userIDForm.addEventListener("submit", userIDFormAction);
+    userConfirmationForm.addEventListener("submit", userConfirmationFormAction);
 
     // ----- UI Changes -----
-    // Navigatioon - ensures the navigation is visble when page is refreshed
+    // Navigation - ensures the navigation is visble when page is refreshed
     // from a location other than the top of the page
     changeActiveSection(window.scrollY);
     // Header
